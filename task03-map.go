c0d29ebd1e5d7c8bf289107f5c8f216b41177d62
Dmitriy Bolshakov <bolshakovdmitry@bk.ru>
2022-05-08 16:06:29 +0500

    edit tast 3

--- task03-map.go
+++ task03-map.go
@@ -1,17 +1,16 @@
 package homework
 
 func sortMapValues(input map[int]string) (result []string) {
-	var in = map[int]string{2: "a", 0: "b", 1: "c"}
-	keys := make([]int, 0, len(in))
-	for k := range in {
+	keys := make([]int, 0, len(input))
+	for k := range input {
 		keys = append(keys, k)
 	}
 	sort.Ints(keys)
 	fmt.Println(keys)
 
-	array := make([]string, 0, len(in))
+	array := make([]string, 0, len(input))
 	for _, k := range keys {
-		array = append(array, in[k])
+		array = append(array, input[k])
 	}
 	return array
 }
