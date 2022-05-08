cfc49c92cfbdbca1c829ea937bab437ab206f634
Dmitriy Bolshakov <bolshakovdmitry@bk.ru>
2022-05-08 05:46:18 +0500

    first task

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	var sum float32 = 0
+	for _, s := range input {
+		sum += s
+	}
+	return sum
 }
