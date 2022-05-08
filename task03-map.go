e28221a3586fc133b769406fc73ad0d27abe820a
Dmitriy Bolshakov <bolshakovdmitry@bk.ru>
2022-05-08 16:08:09 +0500

    fix import

--- task03-map.go
+++ task03-map.go
@@ -1,12 +1,15 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
 	keys := make([]int, 0, len(input))
 	for k := range input {
 		keys = append(keys, k)
 	}
 	sort.Ints(keys)
-	fmt.Println(keys)
 
 	array := make([]string, 0, len(input))
 	for _, k := range keys {
